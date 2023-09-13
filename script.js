0e4566aece7aaf42a73dd287de47947582b3e4ec
NICOLLAS PEREIRA REZENDE <nicollaspereira025@gmail.com>
2023-09-12 22:21:29 -0300

    v0.0.2

--- script.js
+++ script.js
@@ -1,13 +1,31 @@
+function scrollToTop() {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+    }
+
+// Adicione um evento de rolagem para mostrar ou ocultar o botão "Voltar ao Topo"
+window.addEventListener('scroll', function () {
+    const scrollTopButton = document.getElementById('scrollTop');
+    if (window.scrollY > 200) {
+        scrollTopButton.style.display = 'block';
+    } else {
+        scrollTopButton.style.display = 'none';
+    }
+});
+
+// Código para rolagem suave ao clicar nos links de navegação
 document.querySelectorAll("nav a").forEach((link) => {
     link.addEventListener("click", function (e) {
-    e.preventDefault();
+        e.preventDefault();
 
-    const href = this.getAttribute("href");
-    const offsetTop = document.querySelector(href).offsetTop;
+        const href = this.getAttribute("href");
+        const offsetTop = document.querySelector(href).offsetTop;
 
-    scroll({
-        top: offsetTop,
-        behavior: "smooth",
-    });
+        scroll({
+            top: offsetTop,
+            behavior: "smooth",
+        });
     });
 });
