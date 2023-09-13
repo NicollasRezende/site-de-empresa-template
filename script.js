0ad58adf96720af93feedcf6d47e04da495eaeca
NICOLLAS PEREIRA REZENDE <nicollaspereira025@gmail.com>
2023-09-12 21:29:21 -0300

    v0.0.1

---
+++ script.js
@@ -0,0 +1,13 @@
+document.querySelectorAll("nav a").forEach((link) => {
+    link.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    const href = this.getAttribute("href");
+    const offsetTop = document.querySelector(href).offsetTop;
+
+    scroll({
+        top: offsetTop,
+        behavior: "smooth",
+    });
+    });
+});
